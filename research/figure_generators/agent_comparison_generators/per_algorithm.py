c032343e205598fe33c7678df3b0c4d2f56a12fa
James Ekern <jamesekern@Jamess-MacBook-Air.local>
2026-07-24 18:44:18 -0500

    Finish generating all graphs. Next up is compiling important data into tables

--- research/figure_generators/agent_comparison_generators/per_algorithm.py
+++ research/figure_generators/agent_comparison_generators/per_algorithm.py
@@ -1,7 +1,10 @@
 # any graphs that are the same for every algorithm, but do not combine
 
 import ast
+import sys
 from pathlib import Path
+
+sys.path.append(str(Path(__file__).resolve().parent.parent.parent))
 
 import camel_converter
 import pandas as pd
@@ -17,6 +20,8 @@ from graph_config import (
     WIDTH,
 )
 from graph_utils import save_multi_line
+
+from metric_generators.algorithm_metrics import AlgorithmMetrics
 
 # ========== GRAPHING GUESS FREQUENCY BY GUESS ========== #
 
@@ -80,17 +85,15 @@ condition_dfs = {
     "roll": ALGORITHM_DFS_BY_ROLL,
     "peek_average": ALGORITHM_DFS_BY_PEEKS,
 }
+graphs = [
+    "average_payout",
+    "exact_hit_rate",
+    "mean_absolute_error",
+    "average_operations",
+]
 for condition, dfs in condition_dfs.items():
-    graphs = [
-        # y_values, graph title)
-        "average_payout",
-        "exact_hit_rate",
-        "mean_absolute_error",
-        "average_operations",
-    ]
-
     for y_value in graphs:
-        title = f"{y_value.replace('_', ' ').title()} per {condition.title()}"
+        title = f"{y_value.replace('_', ' ').title()} for {condition.title()}"
         save_multi_line(
             dfs,
             condition,
@@ -98,5 +101,50 @@ for condition, dfs in condition_dfs.items():
             title,
             condition.replace("_", " ").title(),
             y_value.replace("_", " ").title(),
-            f"{AGENT_COMPARIONS_FIG_DIR}/per_algorithm/{camel_converter.to_snake(title)}.png",
+            f"{AGENT_COMPARIONS_FIG_DIR}/per_algorithm/{y_value}_for_{condition}.png",
         )
+# ========== GRAPHING METRIC VS METRIC ie PEEK VARIANCE VS AVERAGE PAYOUT
+# going to use algorithm metrics to pull the metrics that I didn't automatically generate
+graphs = [
+    "average_payout",
+    "exact_hit_rate",
+    "mean_absolute_error",
+    "average_operations",
+    "average_deviation_guess",
+]
+metrics_to_compare = ["peek_variance", "roll", "total_operations", "payout"]
+metrics_dfs = {
+    # metric: [dfs]
+}
+
+for i in metrics_to_compare:
+    metrics_dfs[i] = {}
+
+for algorithm in ALGORITHMS:
+    path = f"/Users/jamesekern/pythonProjects/gamblint/research/data/raw/{algorithm}_10000.parquet"
+    algorithm_metrics = AlgorithmMetrics(path)
+    for metric_x in metrics_to_compare:
+        metric_x_df = pd.DataFrame(
+            algorithm_metrics.filtered(metric_x, algorithm_metrics.all)
+        )
+        metrics_dfs[metric_x][algorithm] = metric_x_df
+
+for metric_x in metrics_to_compare:
+    for metric_y in graphs:  # bad naming convention, but take what you can get
+        if (
+            metric_y != metric_x
+            and not (metric_x == "payout" and metric_y == "average_payout")
+            and not (
+                metric_x == "total_operations" and metric_y == "average_operations"
+            )
+        ):
+            title = f"{metric_y.replace('_', ' ').title()} for {metric_x.replace('_', ' ').title()}"
+            save_multi_line(
+                metrics_dfs[metric_x],
+                metric_x,
+                metric_y,
+                title,
+                metric_x,
+                metric_y,
+                f"{AGENT_COMPARIONS_FIG_DIR}/per_algorithm/{camel_converter.to_snake(f'{metric_y}_for_{metric_x}')}.png",
+            )
